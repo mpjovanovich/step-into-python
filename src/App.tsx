f493ef7b8fce100e813047aa616ffed7214a39a5
Jonny Danger <mpjovanovich@gmail.com>
2024-10-14 06:49:03 -0400

    In good shape. Moving on to multistep program.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { QuizQuestion } from "./components/QuizQuestion";
 
 const App = () => {
+  // Since there may be several answers, we'll use an array instead of one
+  // string to store state for the user's answers.
+  const [userAnswers, setUserAnswers] = useState<string[]>([]);
+  const [isCorrect, setIsCorrect] = useState(false);
   const [step, setStep] = useState(1);
 
   // This will be encoded in a JSON serialized map.
@@ -9,12 +13,23 @@ const App = () => {
   const templateCode = `print("Hello, {{sun}}!")
 print("Hello, moon!")
 `;
-
+  // const codeMapString = JSON.stringify(codeMap);
   const codeMap: Map<Number, String> = new Map([[1, templateCode]]);
 
-  // Serialize the map into a string.
-  const codeMapString = JSON.stringify(codeMap);
-  // console.log(Object.fromEntries(codeMap));
+  const handleCheckAnswer = () => {
+    // Answers to the current question.
+    const questionTemplate = codeMap.get(step)?.toString() ?? "";
+    const answers =
+      questionTemplate.match(/{{(.*?)}}/g)?.map((p) => p.slice(2, -2)) || [];
+
+    // Check against user responses.
+    const isCorrect =
+      answers.length === userAnswers.length &&
+      answers.every((answer, i) => answer === userAnswers[i]);
+
+    setIsCorrect(isCorrect);
+    console.log("Answer is", isCorrect ? "correct" : "incorrect");
+  };
 
   return (
     <div className="App" style={styles.app}>
@@ -24,7 +39,29 @@ print("Hello, moon!")
           <h2>Development Code</h2>
           <QuizQuestion
             questionTemplate={codeMap.get(step)?.toString() ?? ""}
+            userAnswers={userAnswers}
+            setUserAnswers={setUserAnswers}
           />
+
+          {/* Show if the answer is incorrect or incomplete. */}
+          {!isCorrect && <button onClick={handleCheckAnswer}>Check</button>}
+
+          {/* Show if the answer is correct and there are more questions. */}
+          {isCorrect && step < codeMap.size && (
+            <button
+              onClick={() => {
+                setStep(step + 1);
+                setIsCorrect(false);
+              }}
+            >
+              Next
+            </button>
+          )}
+
+          {/* Show submit button if it's the last question. */}
+          {isCorrect && step === codeMap.size && (
+            <button onClick={handleCheckAnswer}>Submit</button>
+          )}
         </div>
         <div style={styles.column}>
           <h2>Complete Program</h2>
--- src/components/QuizQuestion.tsx
+++ src/components/QuizQuestion.tsx
@@ -2,33 +2,58 @@ import React, { useState } from "react";
 
 interface QuizQuestionProps {
   questionTemplate: string;
-  // onAnswer: (answer: string) => void;
+  userAnswers: string[];
+  setUserAnswers: (answers: string[]) => void;
+  // onCheckAnswer: (correct: boolean) => void;
 }
 
-const QuizQuestion = ({ questionTemplate }: QuizQuestionProps) => {
-  const [textInput, setTextInput] = useState("");
-  const InputField = (
-    <input
-      type="text"
-      value={textInput}
-      onChange={(e) => setTextInput(e.target.value)}
-      style={{ width: "120px" }}
-    />
-  );
+/* **********************************************************************
+ * This has the fill-in-the-blank code functionality, as well as the
+ * "Check" button to see if the answer is correct.
+ ************************************************************************/
+const QuizQuestion = ({
+  questionTemplate,
+  userAnswers,
+  setUserAnswers,
+}: // onCheckAnswer,
+QuizQuestionProps) => {
+  // These are the actual answers provided via the template.
+  const answers =
+    questionTemplate.match(/{{(.*?)}}/g)?.map((p) => p.slice(2, -2)) || [];
+
+  const checkAnswer = () => {
+    const isCorrect =
+      answers.length === userAnswers.length &&
+      answers.every((answer, i) => answer === userAnswers[i]);
+    // onCheckAnswer(isCorrect);
+  };
 
   const renderTemplate = () => {
     // Split the template into parts, ignoring the {{}}
     const parts = questionTemplate.split(/{{.*?}}/);
 
-    // The values in the {{}} are the correct answers.
-    const answers =
-      questionTemplate.match(/{{(.*?)}}/g)?.map((p) => p.slice(2, -2)) || [];
-
     // Stubs in an input field for each {{}} in the template
-    return parts.map((part, index) => (
-      <React.Fragment key={index}>
+    return parts.map((part, i) => (
+      <React.Fragment key={i}>
+        {/* The readonly part of the template */}
         {part}
-        {index < parts.length - 1 && InputField}
+        {/* The input field for the user's answer */}
+        {/* Don't render for the last part in the template */}
+        {i < answers.length && (
+          <input
+            type="text"
+            value={userAnswers[i] || ""}
+            onChange={(e) => {
+              // This spreads the current answers into a new array so that we
+              // can mutate it, updates the answer at the current index, and
+              // sets the new array as state for the user's answers.
+              const newAnswers = [...userAnswers];
+              newAnswers[i] = e.target.value;
+              setUserAnswers(newAnswers);
+            }}
+            style={{ width: "120px" }}
+          />
+        )}
       </React.Fragment>
     ));
   };
@@ -37,6 +62,7 @@ const QuizQuestion = ({ questionTemplate }: QuizQuestionProps) => {
     <div>
       <pre>
         <code>{renderTemplate()}</code>
+        {/* <button onClick={checkAnswer}>Check</button> */}
       </pre>
     </div>
   );
