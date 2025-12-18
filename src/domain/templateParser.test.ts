7ae5107951fbd3bccd67b31e38c3ffea59476a1e
Mike Jovanovich <mpjovanovich@gmail.com>
2025-12-18 12:56:25 -0500

    Test fixture json added for upcoming template tests.

--- src/domain/templateParser.test.ts
+++ src/domain/templateParser.test.ts
@@ -1,7 +1,14 @@
 import { describe, expect, it } from "vitest";
-import { getStepCount } from "./templateParser";
+import testTemplate from "./fixtures/test-template.json";
+import {
+  getStepCount,
+  parseTemplate,
+  type ParseOptions,
+} from "./templateParser";
 
-describe("getStepCount", () => {
+const testTemplateString = testTemplate.template.join("\n");
+
+describe("step count tests", () => {
   it.each([
     {
       description: "single step, single line",
@@ -21,5 +28,17 @@ describe("getStepCount", () => {
   ])("$description", ({ template, expected }) => {
     const result = getStepCount(template);
     expect(result).toEqual(expected);
+  });
+});
+
+describe("template parsing tests", () => {
+  it("should do something", () => {
+    const templateOptions: ParseOptions = {
+      title: "TEST TITLE",
+      questionTemplate: testTemplateString,
+      currentStep: 1,
+    };
+    const result = parseTemplate(templateOptions);
+    expect(result).toBeDefined();
   });
 });
