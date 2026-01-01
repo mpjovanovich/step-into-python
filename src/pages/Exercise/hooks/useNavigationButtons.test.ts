5071d463910d32dee89659217268f598d19b13b5
Mike Jovanovich <mpjovanovich@gmail.com>
2026-01-01 14:38:02 -0500

    Added some tests

---
+++ src/pages/Exercise/hooks/useNavigationButtons.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, it, vi } from "vitest";
+import { useNavigationButtons } from "./useNavigationButtons";
+
+describe("navigation buttons", () => {
+  it("creates all three buttons with correct text", () => {
+    const { buttons } = useNavigationButtons({
+      step: 1,
+      finalStep: 1,
+      checkAnswerResults: [],
+      onPrevious: () => {},
+      onNext: () => {},
+      onSubmit: () => {},
+    });
+    expect(buttons.length).toBe(3);
+    const previousButton = buttons.find((button) => button.text === "Previous");
+    expect(previousButton).toBeDefined();
+    const nextButton = buttons.find((button) => button.text === "Next");
+    expect(nextButton).toBeDefined();
+    const submitButton = buttons.find((button) => button.text === "Submit");
+    expect(submitButton).toBeDefined();
+  });
+
+  it("wires up click handlers for buttons", () => {
+    const onPrevious = vi.fn();
+    const onNext = vi.fn();
+    const onSubmit = vi.fn();
+
+    const { buttons } = useNavigationButtons({
+      step: 1,
+      finalStep: 1,
+      checkAnswerResults: [],
+      onPrevious,
+      onNext,
+      onSubmit,
+    });
+
+    // Get "previous" button
+    const previousButton = buttons.find((button) => button.text === "Previous");
+    previousButton?.onClick();
+    expect(onPrevious).toHaveBeenCalled();
+
+    // Get "next" button
+    const nextButton = buttons.find((button) => button.text === "Next");
+    nextButton?.onClick();
+    expect(onNext).toHaveBeenCalled();
+
+    // Get "submit" button
+    const submitButton = buttons.find((button) => button.text === "Submit");
+    submitButton?.onClick();
+    expect(onSubmit).toHaveBeenCalled();
+  });
+
+  // TODO: enabled state testing
+
+  // TODO: visible state testing
+});
