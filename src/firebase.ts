72ec2cb68134afb99ea2c708f0c3803625486fec
Mike Jovanovich <mpjovanovich@gmail.com>
2026-01-02 14:04:18 -0500

    Made firebase config generic

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDOwKQDJYftjR8vmEzF4klMeP8VLZW9yB8",
-  authDomain: "stepintopython.firebaseapp.com",
-  projectId: "stepintopython",
-  storageBucket: "stepintopython.firebasestorage.app",
-  messagingSenderId: "805215190705",
-  appId: "1:805215190705:web:1590c0f86e90a1ad68ec90",
-};
+import { getFirestore } from "firebase/firestore";
+import FIREBASE_CONFIG from "./firebase.config";
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(FIREBASE_CONFIG);
 
 // Initialize Firestore
 export const db = getFirestore(app);
