1a1c7d418e02e2e785946918c9a953c8ac892435
Mike Jovanovich <mpjovanovich@gmail.com>
2026-01-03 18:58:34 -0500

    firestore rules and tests updated. User schema changed - needs to have PK set to uid from auth user

--- firestore.rules.test.js
+++ firestore.rules.test.js
@@ -1,10 +1,11 @@
 import {
   assertFails,
+  assertSucceeds,
   initializeTestEnvironment,
 } from "@firebase/rules-unit-testing";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import fs from "fs";
-import { beforeAll, describe, it } from "vitest";
+import { beforeAll, beforeEach, describe, it } from "vitest";
 
 /*
  * This script requires the firebase emulator to be running.
@@ -27,12 +28,111 @@ describe("firestore database", () => {
     });
   });
 
-  it("DEBUG: should deny all reads and writes", async () => {
-    const authContext = testEnv.unauthenticatedContext();
-    const db = authContext.firestore();
+  describe("for exercises collection", () => {
+    describe("for unauthenticated user", () => {
+      let authContext;
+      let db;
 
-    // Test that read fails
-    const docRef = doc(db, "exercises", "1");
-    await assertFails(getDoc(docRef));
+      beforeEach(async () => {
+        await testEnv.clearFirestore();
+        authContext = testEnv.unauthenticatedContext();
+        db = authContext.firestore();
+      });
+
+      it("should deny reads", async () => {
+        await assertFails(getDoc(doc(db, "exercises", "1")));
+      });
+
+      it("should deny writes", async () => {
+        await assertFails(setDoc(doc(db, "exercises", "1"), { test: true }));
+      });
+    });
+
+    describe("for authenticated user", () => {
+      let authContext;
+      let db;
+
+      beforeEach(async () => {
+        await testEnv.clearFirestore();
+        authContext = testEnv.authenticatedContext("user1");
+        db = authContext.firestore();
+      });
+
+      it("should allow reads", async () => {
+        await assertSucceeds(getDoc(doc(db, "exercises", "1")));
+      });
+
+      it("should deny writes", async () => {
+        await assertFails(setDoc(doc(db, "exercises", "1"), { test: true }));
+      });
+    });
+  });
+
+  describe("for users collection", () => {
+    describe("for unauthenticated user", () => {
+      let authContext;
+      let db;
+
+      beforeEach(async () => {
+        await testEnv.clearFirestore();
+        authContext = testEnv.unauthenticatedContext();
+        db = authContext.firestore();
+      });
+
+      it("should deny reads", async () => {
+        await assertFails(getDoc(doc(db, "users", "1")));
+      });
+
+      it("should deny writes", async () => {
+        await assertFails(setDoc(doc(db, "users", "1"), { test: true }));
+      });
+    });
+
+    describe("for authenticated user", () => {
+      let authContext;
+      let db;
+
+      beforeEach(async () => {
+        await testEnv.clearFirestore();
+        authContext = testEnv.authenticatedContext("user1");
+        db = authContext.firestore();
+      });
+
+      it("should deny reads to other users", async () => {
+        await assertFails(getDoc(doc(db, "users", "user2")));
+      });
+
+      it("should deny writes to other users", async () => {
+        await assertFails(setDoc(doc(db, "users", "user2"), { test: true }));
+      });
+
+      it("should allow reads to own document", async () => {
+        await assertSucceeds(getDoc(doc(db, "users", "user1")));
+      });
+
+      it("should not allow updates to own document if field is not completedExercises", async () => {
+        await testEnv.withSecurityRulesDisabled(async (context) => {
+          const adminDb = context.firestore();
+          await setDoc(doc(adminDb, "users", "user1"), { test: true });
+        });
+        await assertFails(
+          updateDoc(doc(db, "users", "user1"), { test: false })
+        );
+      });
+
+      it("should allow updates to own document if field is completedExercises", async () => {
+        await testEnv.withSecurityRulesDisabled(async (context) => {
+          const adminDb = context.firestore();
+          await setDoc(doc(adminDb, "users", "user1"), {
+            completedExercises: ["exercise1"],
+          });
+        });
+        await assertSucceeds(
+          updateDoc(doc(db, "users", "user1"), {
+            completedExercises: ["exercise1", "exercise2"],
+          })
+        );
+      });
+    });
   });
 });
